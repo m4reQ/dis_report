88e1158db4c71378b4aed6aba1f68c4873fa0fa7
m4reQ <m4reQ@users.noreply.github.com>
2022-08-08 23:26:35 +0200

    Use airium for generating html

--- dis_report.py
+++ dis_report.py
@@ -1,12 +1,9 @@
 import dis
 import html
-import io
-import os
 import time
 import types
-import typing as t
 
-_MAX_REPR_LEN = 64
+import airium
 
 _FLAGS_DESCRPTIONS = {
     'OPTIMIZED': 'The code object is optimized, using fast locals.',
@@ -20,96 +17,125 @@ _FLAGS_DESCRPTIONS = {
     'ITERABLE_COROUTINE': 'The flag is used to transform generators into generator-based coroutines. Generator objects with this flag can be used in await expression, and can yield from coroutine objects. See PEP 492 for more details.',
     'ASYNC_GENERATOR': 'The flag is set when the code object is an asynchronous generator function. When the code object is executed it returns an asynchronous generator object. See PEP 525 for more details.'}
 
-def _create_html_table(data: t.Iterable[t.Any], row_constructor: t.Callable[[t.Any, int], str]) -> str:
-    return ''.join(row_constructor(x, i) for i, x in enumerate(data))
-
-def _consts_row_constructor(val: t.Any, index: int) -> str:
-    if isinstance(val, types.CodeType):
-        url = f'#{html.escape(val.co_name)}'
-        _repr = html.escape(val.co_name)
-        return f'<tr>\n<td>{index}</td>\n<td><a href={url}>{_repr}</a></td>\n<td>{type(val).__name__}</td>\n</tr>\n'
-
-    _repr = html.escape(repr(val))
-    return f'<tr>\n<td>{index}</td>\n<td><a class="value">{_repr}</a></td>\n<td>{type(val).__name__}</td>\n</tr>\n'
-
-def _vars_row_constructor(val: t.Any, index: int) -> str:
-    return f'<tr>\n<td>{index}</td>\n<td>{html.escape(val)}</td>\n'
-
-def _instruction_row_constructor(inst: t.Any, _) -> str:
-    if not isinstance(inst, dis.Instruction):
-        raise TypeError('inst agrgument must be of type dis.Instruction.')
-
-    res = io.StringIO()
-    write = res.write
-    write('<tr>\n')
-
-    if inst.starts_line:
-        write(f'<td><a class="lineno">{inst.starts_line}</a></td>\n')
-    else:
-        write('<td></td>\n')
-
-    write(f'<td>')
-
-    if inst.is_jump_target:
-        write(f'<a id=jump_{inst.offset}><u>{inst.offset}</u></a>\n')
-    else:
-        write(f'{inst.offset}')
-
-    write(f'</td>\n<td><a class="opcode">{inst.opname}</a></td>\n')
-
-    if inst.arg:
-        write(f'<td>{inst.arg}</td>\n<td><a class="value"')
-
-        if inst.argrepr.startswith('to '):
-            res.write(f' href=#jump_{inst.argval}')
-
-        write(f'>({html.escape(inst.argrepr)})</a></td>\n')
-    else:
-        write('<td></td>\n<td></td>\n')
-
-    write('</tr>\n')
-
-    return res.getvalue()
-
-def _create_html_flags_text(code_obj: types.CodeType) -> str:
-    flags = dis.pretty_flags(code_obj.co_flags).split(', ')
-
-    result = io.StringIO()
-    for flag in flags:
-        desc = _FLAGS_DESCRPTIONS.get(flag, None)
-        if desc is None:
-            result.write(f'{html.escape(flag)}, ')
-        else:
-            result.write(f'<span title="{html.escape(desc)}">{html.escape(flag)}</span>, ')
-
-    return result.getvalue().removesuffix(', ')
-
 def generate_report_html(code_obj: types.CodeType) -> str:
     start = time.perf_counter()
-    fp = os.path.join(os.path.split(__file__)[0], 'template.html')
-    with open(fp) as f:
-        template = f.read()
 
-    doc = template % (
-        code_obj.co_name,
-        code_obj.co_name,
-        code_obj.co_filename,
-        code_obj.co_filename,
-        _create_html_flags_text(code_obj),
-        code_obj.co_stacksize,
-        _create_html_table(
-            code_obj.co_consts,
-            _consts_row_constructor),
-        _create_html_table(
-            code_obj.co_names,
-            _vars_row_constructor),
-        _create_html_table(
-            code_obj.co_varnames,
-            _vars_row_constructor),
-        _create_html_table(
-            dis.get_instructions(code_obj),
-            _instruction_row_constructor))
+    co_name = code_obj.co_name
+    co_filename = code_obj.co_filename
+
+    doc = airium.Airium()
+    doc('<!DOCTYPE html>')
+    with doc.html(lang='en'):
+        with doc.head():
+            doc.meta(charset='utf-8')
+            doc.title(_t='Dissasembly report')
+            doc.link(rel='stylesheet', href='style.css')
+
+        with doc.body():
+            doc.h3(id='title', _t='DISASSEMBLY REPORT OF OBJECT')
+            doc.h1(id='obj_name', _t=co_name)
+            doc.br()
+
+            with doc.div(id='info'):
+                doc('NAME: ')
+                doc.span(klass='attrib', _t=co_name)
+                doc.br()
+
+                doc('FILEPATH: ')
+                doc.a(klass='attrib', href=f'file:///{co_filename}', _t=co_filename)
+                doc.br()
+
+                doc('COMPILER FLAGS: ')
+                flags = dis.pretty_flags(code_obj.co_flags).split(', ')
+                with doc.span(klass='attrib'):
+                    for flag in flags:
+                        desc = _FLAGS_DESCRPTIONS.get(flag, None)
+                        if desc is None:
+                            doc(html.escape(flag))
+                        else:
+                            doc.span(title=html.escape(desc), _t=html.escape(flag))
+
+                doc.br()
+
+                doc('STACK SIZE: ')
+                doc.span(klass='attrib', _t=str(code_obj.co_stacksize))
+                doc.br()
+
+            doc.hr()
+
+            doc.span(klass='header', _t='Constants')
+            with doc.table():
+                with doc.thead().tr():
+                    doc.th(_t='INDEX')
+                    doc.th(_t='VALUE')
+                    doc.th(_t='TYPE')
+
+                with doc.tbody():
+                    for i, val in enumerate(code_obj.co_consts):
+                        with doc.tr():
+                            doc.td(_t=str(i))
+                            doc.td(_t=html.escape(repr(val)))
+                            doc.td(_t=html.escape(type(val).__name__))
+
+            doc.hr()
+
+            doc.span(klass='header', _t='Variables')
+            with doc.table():
+                with doc.thead().tr():
+                    doc.th(_t='INDEX')
+                    doc.th(_t='NAME')
+
+                with doc.tbody():
+                    for i, name in enumerate(code_obj.co_varnames):
+                        with doc.tr():
+                            doc.td(_t=str(i))
+                            doc.td(_t=name)
+
+            doc.hr()
+
+            doc.span(klass='header', _t='Bytecode')
+            with doc.table():
+                with doc.thead().tr():
+                    doc.th(_t='LINE')
+                    doc.th(_t='OP NUMBER')
+                    doc.th(_t='OPCODE')
+                    doc.th(_t='ARGUMENT', colspan=2)
+
+                with doc.tbody():
+                    for inst in dis.get_instructions(code_obj):
+                        with doc.tr():
+                            with doc.td():
+                                if inst.starts_line is not None:
+                                    doc.span(klass='lineno', _t=str(inst.starts_line))
+
+                            with doc.td():
+                                if inst.is_jump_target:
+                                    with doc.span(id=f'jump_{inst.offset}'):
+                                        doc.u(_t=str(inst.offset))
+                                else:
+                                    doc(str(inst.offset))
+
+                            with doc.td():
+                                doc.span(klass='opcode', _t=inst.opname)
+
+                            if inst.arg is not None:
+                                doc.td(_t=str(inst.arg))
+
+                                with doc.td():
+                                    arg_repr = html.escape(inst.argrepr)
+
+                                    if inst.argrepr.startswith('to '):
+                                        doc.a(klass='value', href=f'#jump_{inst.argval}', _t=arg_repr)
+                                    else:
+                                        doc.span(klass='value', _t=arg_repr)
+
+            doc.hr()
+            doc.h3(id='title', _t='END OF REPORT')
 
     print(f'Report document generated in {time.perf_counter() - start} seconds.')
 
-    return doc
+    return str(doc)
+
+if __name__ == '__main__':
+    with open('result.html', 'w+') as f:
+        f.write(generate_report_html(generate_report_html.__code__))
